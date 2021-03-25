05f93cfaf28e7f3f08cccb2722eb333aed4a38a1
HellBlazer616 <rifat.hossain.dhrubo@gmail.com>
2021-03-25 21:58:04 +0600

    more ui feedback 💡💡🚦🚦⚡⚡

--- components/home/ShoutOutShowCase.tsx
+++ components/home/ShoutOutShowCase.tsx
@@ -30,7 +30,61 @@ interface DisLikeVariable extends Variable {
 }
 const ShoutOutShowCase: FC<Props> = ({ post }) => {
   const { user } = useContext(AuthContext);
+
+  const disLikedByCurrentUser = useMemo(() => {
+    if (post.disLikedByUsers == null || user?.uid == null) return false;
+
+    const result = post.disLikedByUsers.find(
+      (userData) => userData.uid === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.disLikedByUsers, user?.uid]);
+
+  const likedByCurrentUser = useMemo(() => {
+    if (post.likedByUsers == null || user?.uid == null) return false;
+
+    const result = post.likedByUsers.find(
+      (userData) => userData.uid === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.likedByUsers, user?.uid]);
+
+  const commentedByCurrentUser = useMemo(() => {
+    if (post.comments == null || user?.uid == null) return false;
+
+    const result = post.comments.find(
+      (userData) => userData.authorId === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.comments, user?.uid]);
+
+  const dislikedBy = useMemo(() => {
+    if (post.disLikedByUsers == null) return 0;
+
+    return post.disLikedByUsers.length;
+  }, [post.disLikedByUsers]);
+
+  const likedBy = useMemo(() => {
+    if (post.likedByUsers == null) return 0;
+
+    return post.likedByUsers.length;
+  }, [post.likedByUsers]);
+
+  const commentedBy = useMemo(() => {
+    if (post.comments == null) return 0;
+
+    return post.comments.length;
+  }, [post.comments]);
+
+  const formattedTime = useMemo(() => formatDate(Date.parse(post.createdAt)), [
+    post.createdAt,
+  ]);
+
   const queryClient = useQueryClient();
+
   const { mutateAsync: likeMutation } = useMutation<Post, any, LikeVariable>(
     (createComment) => axios.put('/api/posts', createComment),
     {
@@ -121,27 +175,6 @@ const ShoutOutShowCase: FC<Props> = ({ post }) => {
     disLikePost();
   };
 
-  const likedBy = useMemo(() => {
-    if (post.likedByUsers == null) return 0;
-
-    return post.likedByUsers.length;
-  }, [post.likedByUsers]);
-
-  const dislikedBy = useMemo(() => {
-    if (post.disLikedByUsers == null) return 0;
-
-    return post.disLikedByUsers.length;
-  }, [post.disLikedByUsers]);
-
-  const commentedBy = useMemo(() => {
-    if (post.comments == null) return 0;
-
-    return post.comments.length;
-  }, [post.comments]);
-
-  const formattedTime = useMemo(() => formatDate(Date.parse(post.createdAt)), [
-    post.createdAt,
-  ]);
   return (
     <div tw="mx-auto max-w-prose">
       <Wrapper>
@@ -163,24 +196,26 @@ const ShoutOutShowCase: FC<Props> = ({ post }) => {
           <div>{post.text}</div>
           <div tw="flex flex-wrap justify-end w-full space-x-10">
             <Link href={`/posts/${post._id}`} passHref>
-              <ShoutOutBoxButton as="a">
+              <ShoutOutBoxButton as="a" active={commentedByCurrentUser}>
                 <HiChat />
-                <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-xs bg-accent-500 rounded-full">
-                  {commentedBy}
-                </span>
+                <HoverIcon>{commentedBy}</HoverIcon>
               </ShoutOutBoxButton>
             </Link>
-            <ShoutOutBoxButton type="button" onClick={handleLike}>
+            <ShoutOutBoxButton
+              type="button"
+              onClick={handleLike}
+              active={likedByCurrentUser}
+            >
               <HiThumbUp />
-              <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-sm bg-accent-500 rounded-full">
-                {likedBy}
-              </span>
+              <HoverIcon>{likedBy}</HoverIcon>
             </ShoutOutBoxButton>
-            <ShoutOutBoxButton type="button" onClick={handleDisLike}>
+            <ShoutOutBoxButton
+              type="button"
+              onClick={handleDisLike}
+              active={disLikedByCurrentUser}
+            >
               <HiThumbDown />
-              <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-sm bg-accent-500 rounded-full">
-                {dislikedBy}
-              </span>
+              <HoverIcon>{dislikedBy}</HoverIcon>
             </ShoutOutBoxButton>
             <Link href={`/posts/${post._id}`} passHref>
               <ShoutOutBoxButton as="a">
@@ -206,6 +241,19 @@ const Wrapper = styled.div`
   grid-template-columns: auto 1fr;
 `;
 
-const ShoutOutBoxButton = styled.button`
-  ${tw`relative inline-flex items-center px-3 py-3 text-white text-lg font-medium bg-accent-600 hover:bg-accent-700 border border-transparent rounded-full focus:outline-none shadow-sm focus:ring-accent-500 focus:ring-offset-primary-100 focus:ring-offset-2 focus:ring-2`}
+type StyledProps = {
+  active?: boolean;
+};
+
+const ShoutOutBoxButton = styled.button<StyledProps>`
+  ${tw`relative inline-flex items-center mb-3 px-2 py-2 text-base font-medium bg-accent-600 hover:bg-accent-700 border border-transparent rounded-full focus:outline-none shadow-sm focus:ring-accent-500 focus:ring-offset-primary-100 focus:ring-offset-2 focus:ring-2`}
+
+  ${(props) =>
+    props.active == null || props.active === false
+      ? tw`text-primary-400`
+      : tw`text-white`}
+`;
+
+const HoverIcon = styled.span`
+  ${tw`absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-xs bg-accent-500 rounded-full`}
 `;
--- components/posts/ShoutOutShowCaseWithComment.tsx
+++ components/posts/ShoutOutShowCaseWithComment.tsx
@@ -33,6 +33,59 @@ interface DisLikeVariable extends Variable {
 
 const ShoutOutShowCase: FC<Props> = ({ post }) => {
   const { user } = useContext(AuthContext);
+
+  const disLikedByCurrentUser = useMemo(() => {
+    if (post.disLikedByUsers == null || user?.uid == null) return false;
+
+    const result = post.disLikedByUsers.find(
+      (userData) => userData.uid === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.disLikedByUsers, user?.uid]);
+
+  const likedByCurrentUser = useMemo(() => {
+    if (post.likedByUsers == null || user?.uid == null) return false;
+
+    const result = post.likedByUsers.find(
+      (userData) => userData.uid === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.likedByUsers, user?.uid]);
+
+  const commentedByCurrentUser = useMemo(() => {
+    if (post.comments == null || user?.uid == null) return false;
+
+    const result = post.comments.find(
+      (userData) => userData.authorId === user.uid
+    );
+    if (result == null) return false;
+    return true;
+  }, [post.comments, user?.uid]);
+
+  const dislikedBy = useMemo(() => {
+    if (post.disLikedByUsers == null) return 0;
+
+    return post.disLikedByUsers.length;
+  }, [post.disLikedByUsers]);
+
+  const likedBy = useMemo(() => {
+    if (post.likedByUsers == null) return 0;
+
+    return post.likedByUsers.length;
+  }, [post.likedByUsers]);
+
+  const commentedBy = useMemo(() => {
+    if (post.comments == null) return 0;
+
+    return post.comments.length;
+  }, [post.comments]);
+
+  const formattedTime = useMemo(() => formatDate(Date.parse(post.createdAt)), [
+    post.createdAt,
+  ]);
+
   const queryClient = useQueryClient();
   const { mutateAsync: likeMutation } = useMutation<Post, any, LikeVariable>(
     (createComment) => axios.put('/api/posts', createComment),
@@ -124,27 +177,6 @@ const ShoutOutShowCase: FC<Props> = ({ post }) => {
     disLikePost();
   };
 
-  const likedBy = useMemo(() => {
-    if (post.likedByUsers == null) return 0;
-
-    return post.likedByUsers.length;
-  }, [post.likedByUsers]);
-
-  const dislikedBy = useMemo(() => {
-    if (post.disLikedByUsers == null) return 0;
-
-    return post.disLikedByUsers.length;
-  }, [post.disLikedByUsers]);
-
-  const commentedBy = useMemo(() => {
-    if (post.comments == null) return 0;
-
-    return post.comments.length;
-  }, [post.comments]);
-
-  const formattedTime = useMemo(() => formatDate(Date.parse(post.createdAt)), [
-    post.createdAt,
-  ]);
   return (
     <div tw="space-y-3">
       <Wrapper>
@@ -166,24 +198,26 @@ const ShoutOutShowCase: FC<Props> = ({ post }) => {
           <div>{post.text}</div>
           <div tw="flex flex-wrap justify-end w-full space-x-10">
             <Link href={`/posts/${post._id}`} passHref>
-              <ShoutOutBoxButton as="a">
+              <ShoutOutBoxButton as="a" active={commentedByCurrentUser}>
                 <HiChat />
-                <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-xs bg-accent-500 rounded-full">
-                  {commentedBy}
-                </span>
+                <HoverIcon>{commentedBy}</HoverIcon>
               </ShoutOutBoxButton>
             </Link>
-            <ShoutOutBoxButton type="button" onClick={handleLike}>
+            <ShoutOutBoxButton
+              type="button"
+              onClick={handleLike}
+              active={likedByCurrentUser}
+            >
               <HiThumbUp />
-              <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-sm bg-accent-500 rounded-full">
-                {likedBy}
-              </span>
+              <HoverIcon>{likedBy}</HoverIcon>
             </ShoutOutBoxButton>
-            <ShoutOutBoxButton type="button" onClick={handleDisLike}>
+            <ShoutOutBoxButton
+              type="button"
+              onClick={handleDisLike}
+              active={disLikedByCurrentUser}
+            >
               <HiThumbDown />
-              <span tw="absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-sm bg-accent-500 rounded-full">
-                {dislikedBy}
-              </span>
+              <HoverIcon>{dislikedBy}</HoverIcon>
             </ShoutOutBoxButton>
             <Link href={`/posts/${post._id}`} passHref>
               <ShoutOutBoxButton as="a">
@@ -211,6 +245,14 @@ const Wrapper = styled.div`
   grid-template-columns: auto 1fr;
 `;
 
-const ShoutOutBoxButton = styled.button`
+type StyledProps = {
+  active?: boolean;
+};
+
+const ShoutOutBoxButton = styled.button<StyledProps>`
   ${tw`relative inline-flex items-center px-3 py-3 text-white text-lg font-medium bg-accent-600 hover:bg-accent-700 border border-transparent rounded-full focus:outline-none shadow-sm focus:ring-accent-500 focus:ring-offset-primary-100 focus:ring-offset-2 focus:ring-2`}
+`;
+
+const HoverIcon = styled.span`
+  ${tw`absolute -right-1 -top-3 inline-flex items-center justify-center w-5 h-5 text-xs bg-accent-500 rounded-full`}
 `;
